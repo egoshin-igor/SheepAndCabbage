5e0218e00834fd0093fcefe9e4823ef564c4b2c7
igor.egoshin <igor.egoshin@mail.ru>
2019-06-01 15:43:04 +0300

    Commit

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public void GoToMenu()
+        {
+            SceneManager.LoadScene( "Menu" );
+        }
+
+        public void GoToGame()
+        {
+            SceneManager.LoadScene( "MainGame" );
+        }
+
+        public void GoToAchievements()
+        {
+            SceneManager.LoadScene( "Achievements" );
+        }
+
+        public void GoToTutorial()
+        {
+            SceneManager.LoadScene( "Tutorial" );
+        }
+
+        public void GoToExit()
+        {
+            Application.Quit();
+        }
+    }
+}
